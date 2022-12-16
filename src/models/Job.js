648b6e5c9bc69153a30139a06287f8872987c168
Alexander Grigoryan <a.grigoryan@inbox.ru>
2022-12-16 12:51:34 +0400

    feat: make server with express, connect mongodb to server and make schema for job model

---
+++ src/models/Job.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const jobSchema = new Schema({
+  company: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  logo: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  new: {
+    type: Schema.Types.Boolean,
+    required: true,
+  },
+  featured: {
+    type: Schema.Types.Boolean,
+    required: true,
+  },
+  position: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  role: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  level: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  postedAt: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  contract: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  location: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  languages: {
+    type: Schema.Types.Array,
+    required: true,
+  },
+  tools: {
+    type: Schema.Types.Array,
+    required: true,
+  },
+});
+
+const Job = mongoose.model("Job", jobSchema);
+
+export default Job;
